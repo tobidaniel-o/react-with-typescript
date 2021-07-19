a7f751347f16fba7011e7928bbd0bff1b3afb235
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-07-19 15:21:38 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import List from "./components/List";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      age: 36,
+      note: "Allergic to staying on the same team.",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited To My Party</h1>
+      <List people={people} />
     </div>
   );
 }
